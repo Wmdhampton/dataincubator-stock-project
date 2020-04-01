e8c0403403d84c96243ec43a212accb563db8f31
Dave Hampton <wmdhampton@gmail.com>
2020-04-01 13:21:06 -0400

    1.4 complete draft

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,9 @@
-
 from flask import Flask
+from flask import Flask, render_template, request, redirect
+from bokeh.plotting import figure, output_file, show
+from bokeh.embed import components
+import quandl
+quandl.ApiConfig.api_key = "visprKjKxPE5TXHoFLw5"
 
 app = Flask(__name__)
 
