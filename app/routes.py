14076d8ea3ab5307973d79e7365a914394c0bfcc
Dave Hampton <wmdhampton@gmail.com>
2020-04-01 12:12:08 -0400

    1.1 complete draft

--- app/routes.py
+++ app/routes.py
@@ -3,7 +3,6 @@ from flask import Flask, render_template, request, redirect
 from bokeh.plotting import figure, output_file, show
 from bokeh.embed import components
 import quandl
-import matplotlib
 quandl.ApiConfig.api_key = "visprKjKxPE5TXHoFLw5"
 
 def getData(ticker):
